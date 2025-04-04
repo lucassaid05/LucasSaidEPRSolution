9561f87a61aa031779bb6d14c51e3dc221a5c212
Lucas Said <lucas.said.g52506@mcast.edu.mt>
2025-04-04 09:58:32 +0200

    Add project files.

---
+++ LucasSaidEDPSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LucasSaidEDPSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
