8d44b73545dd3f223fa728012d593f7ae5d57055
Lucas Said <lucas.said.g52506@mcast.edu.mt>
2025-04-04 19:12:56 +0200

    Finished Task 1

---
+++ LucasSaidEDPSolution/LucasSaidEPRSolution/DataAccess/DataContext/PollDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DataContext
+{
+    internal class PollDbContext : DbContext
+    {
+        public PollDbContext(DbContextOptions<PollDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Poll> Polls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Poll>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Poll>()
+                .Property(p => p.Title)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            modelBuilder.Entity<Poll>()
+                .Property(p => p.DateCreated)
+                .HasDefaultValueSql("GETUTCDATE()");
+        }
+    }
+}
---
+++ LucasSaidEDPSolution/LucasSaidEPRSolution/DataAccess/Repositories/PollRepository.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess.DataContext;
+using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataAccess.Repositories
+{
+    internal class PollRepository
+    {
+        private readonly PollDbContext _context;
+
+        public PollRepository(PollDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreatePoll(string title, string option1Text, string option2Text, string option3Text)
+        {
+            var poll = new Poll
+            {
+                Title = title,
+                Option1Text = option1Text,
+                Option2Text = option2Text,
+                Option3Text = option3Text,
+                Option1VotesCount = 0,
+                Option2VotesCount = 0,
+                Option3VotesCount = 0,
+                DateCreated = DateTime.UtcNow
+            };
+
+            await _context.Polls.AddAsync(poll);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Poll> GetPollByIdAsync(int id)
+        {
+            return await _context.Polls.FindAsync(id);
+        }
+
+        public async Task<List<Poll>> GetAllPollsAsync()
+        {
+            return await _context.Polls.ToListAsync();
+        }
+    }
+}
