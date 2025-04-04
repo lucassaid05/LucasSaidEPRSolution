8f33b827f622f4d66cf62b386cd193d987315724
Lucas Said <lucas.said.g52506@mcast.edu.mt>
2025-04-04 12:22:55 +0200

    Initializing projetcs

--- LucasSaidEDPSolution/LucasSaidEPRSolution/Domain/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Domain.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
